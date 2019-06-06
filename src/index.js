9dad4dffec9100c811ebd7e2e0886c92671d5ed2
透心凉 <329750577@qq.com>
2019-06-06 17:10:40 +0800

    Update index.js
    优化渲染，新老状态比较，只渲染修改部分

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 // 1. 全局渲染方法
-const renderApp = (state) => {
+const renderApp = (newState, oldState = {}) => {
+  if (newState === oldState) return;
+  console.log('renderApp...');
   // 渲染屏幕
-  renderScreen(state.screen);
+  renderScreen(newState.screen, oldState.screen);
   // 渲染按钮
-  renderButton(state.button);
+  renderButton(newState.button, oldState.button);
 }
 
-const renderScreen = (screen) => {
+const renderScreen = (newScreen, oldScreen) => {
+  if(newScreen === oldScreen) return;
+  console.log('renderScreen...');
   // 获取页面元素
   const screenEl = document.querySelector('#screen');
   // 对页面元素进行渲染
-  screenEl.innerHTML = screen.title;
+  screenEl.innerHTML = newScreen.title;
 }
 
-const renderButton = (button) => {
+const renderButton = (newButton, oldButton) => {
+  if(newButton === oldButton) return;
+  console.log('renderButton...');
   // 获取页面元素
   const buttonEl = document.querySelector('#button');
   // 对页面元素进行渲染
-  buttonEl.innerHTML = button.text;
-  buttonEl.className = button.className;
+  buttonEl.innerHTML = newButton.text;
+  buttonEl.className = newButton.className;
 }
 
 // 2. 对状态的修改，交给纯函数
@@ -26,36 +32,41 @@ const changeState = (state, action) => {
   // 对操作类型进行判断
   switch (action.type) {
     case 'PLAY':
-      // 修改播放状态
-      state.isPlaying = true;
-      // 修改按钮
-      state.button = {
-        className: 'stop',
-        text: '停止'
+      return {
+        isPlaying: true,
+          button: {
+            className: 'stop',
+            text: '停止'
+          },
+          screen: {
+            title: action.title
+          }
       }
-      // 修改屏幕文字
-      state.screen.title = action.title;
-      break;
     case 'NEXT':
-      // 修改屏幕文字
-      state.screen.title = action.title;
-      break;
-    case 'STOP':
-      // 修改播放状态
-      state.isPlaying = false;
-      // 修改按钮
-      state.button = {
-        className: 'play',
-        text: '播放'
+      return {
+        ...state,
+        screen: {
+          title: action.title
+        }
       }
-      // 修改屏幕文字
-      state.screen.title = action.title;
-      break;
+    case 'STOP':
+      return {
+        isPlaying: false,
+          button: {
+            className: 'play',
+            text: '播放'
+          },
+          screen: {
+            title: action.title
+          }
+      }
+    default: 
+      return state;
   }
 }
 
 // 3. 定义一个 createStore 的函数，来管理全局状态数据
-const createStore = (changeState) => {
+const createStore = (reducer) => {
   // 初始化数据
   let state = {
     // 播放状态
@@ -75,11 +86,11 @@ const createStore = (changeState) => {
   let listeners = [];
 
   // 获取数据
-  const getStore = () => state;
+  const getState = () => state;
   // 提供一个 dispatch 方法，用来修改数据
   const dispatch = (action) => {
     // 修改状态
-    changeState(state, action);
+    state = reducer(state, action);
     // 调用所有监听
     listeners.forEach(listener => listener());
   };
@@ -89,32 +100,52 @@ const createStore = (changeState) => {
     listeners.push(listener);
   }
 
-  return { getStore, dispatch, subscribe };
+  return {
+    getState,
+    dispatch,
+    subscribe
+  };
 }
 
 // 4. 使用createStore函数来创建数据的管理对象
 const store = createStore(changeState);
 
+// 声明一个老状态
+let oldState = store.getState();
+
 // 当状态发生修改时，执行重新渲染函数
-store.subscribe(()=>{
+store.subscribe(() => {
+  // 获取最新状态
+  const newState = store.getState();
   // 重新渲染的逻辑
-  renderApp(store.getStore());
+  renderApp(newState, oldState);
+  // 更新老状态
+  oldState = newState;
 });
 
 // 5. 全局初始化
-renderApp(store.getStore());
+renderApp(store.getState());
 
 // 6. 对按钮进行监听，修改状态
-document.querySelector('#button').addEventListener('click', (e) =>{
-  if(e.target.tagName !== 'BUTTON') return;
+document.querySelector('#button').addEventListener('click', (e) => {
+  if (e.target.tagName !== 'BUTTON') return;
   // 判断播放状态
-  if(!store.getStore().isPlaying) {
-    store.dispatch({type: 'PLAY', title: '精彩继续...'});
-    setTimeout(()=>{
-      store.dispatch({type: 'NEXT', title: '下一个精彩...'});
+  if (!store.getState().isPlaying) {
+    store.dispatch({
+      type: 'PLAY',
+      title: '精彩继续...'
+    });
+    setTimeout(() => {
+      console.log('测试...');
+      store.dispatch({
+        type: 'NEXT',
+        title: '下一个精彩...'
+      });
     }, 2000);
   } else {
-    store.dispatch({type: 'STOP', title: '源码TV'});
+    store.dispatch({
+      type: 'STOP',
+      title: '源码TV'
+    });
   }
 })
-
